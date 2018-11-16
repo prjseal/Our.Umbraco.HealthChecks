a7991ded6541abfd0c3bee6d873d84c5472d39d7
prjseal <prjseal@gmail.com>
2018-11-16 23:20:59 +0000

    - revisioned

--- src/Our.Umbraco.HealthChecks/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.HealthChecks/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Our.Umbraco.HealthChecks")]
 [assembly: AssemblyDescription("Umbraco Health Check Contribution Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Our.Umbraco")]
+[assembly: AssemblyCompany("Paul Seal, Umbraco Communtiy")]
 [assembly: AssemblyProduct("Our.Umbraco.HealthChecks")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
