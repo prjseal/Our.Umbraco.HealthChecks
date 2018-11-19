a75ae330ffa23b506a0f05fc4ee163418538df72
Laura W <laura@isfluent.com>
2018-11-19 12:20:19 +0000

    Azure-Umbraco configuration checks

---
+++ src/Our.Umbraco.HealthChecks/Checks/Azure/AzureExamineAnalysis.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Web.HealthCheck;
+using Umbraco.Web.HealthCheck.Checks.Config;
+
+namespace Our.Umbraco.HealthChecks.Checks.Azure
+{
+    [HealthCheck("35631050-103B-45A9-AE24-EDF2E1E82DA6", "Azure Examine Compatibility Check",
+        Description = "Checks that examine settings are appropriate for the Azure platform.",
+        Group = "Azure")]
+    public class AzureExamineAnalysis : AbstractConfigCheck
+    {
+        public AzureExamineAnalysis(HealthCheckContext healthCheckContext) : base(healthCheckContext)
+        {
+
+        }
+
+        public override string FilePath
+        {
+            get { return "~/config/ExamineSettings.config"; }
+        }
+
+        public override string XPath
+        {
+            get { return "/Examine/ExamineIndexProviders/providers/add/@directoryFactory"; }
+        }
+
+        public override bool CanRectify
+        {
+            get { return false; }
+        }
+
+        public override ValueComparisonType ValueComparisonType
+        {
+            get { return ValueComparisonType.ShouldEqual; }
+        }
+
+        public override IEnumerable<AcceptableConfiguration> Values => new List<AcceptableConfiguration>
+        {
+            new AcceptableConfiguration { IsRecommended = true, Value = "Examine.LuceneEngine.Directories.TempEnvDirectoryFactory,Examine" },
+            new AcceptableConfiguration { IsRecommended = true, Value = "Examine.LuceneEngine.Directories.TempEnvDirectoryFactory, Examine" },
+            new AcceptableConfiguration { IsRecommended = true, Value = "Examine.LuceneEngine.Directories.SyncTempEnvDirectoryFactory,Examine" },
+            new AcceptableConfiguration { IsRecommended = true, Value = "Examine.LuceneEngine.Directories.SyncTempEnvDirectoryFactory, Examine" }
+        };
+
+        public override string CheckSuccessMessage
+        {
+            get { return $"Examine directory factory is set to '{CurrentValue}'"; }
+        }
+
+        public override string CheckErrorMessage
+        {
+            get { return $"Examine directory factory should be set to '{Values.First(v => v.IsRecommended).Value}', but is currently set to '{CurrentValue}'"; }
+        }
+    }
+}
---
+++ src/Our.Umbraco.HealthChecks/Checks/Azure/AzureTempStorageAnalysis.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+using Umbraco.Core.Services;
+using Umbraco.Web.HealthCheck;
+
+namespace Our.Umbraco.HealthChecks.Checks.Azure
+{
+    [HealthCheck("F9088377-103A-4712-B428-D4AB6E5B2A67", "Azure Temp Storage Config Check",
+    Description = "Checks that temp storage config is appropriate for the Azure platform.",
+    Group = "Azure")]
+    public class AzureTempStorageAnalysis : HealthCheck
+    {
+
+        public AzureTempStorageAnalysis(HealthCheckContext healthCheckContext) : base(healthCheckContext)
+        {
+        }
+
+        public override IEnumerable<HealthCheckStatus> GetStatus()
+        {
+            //return the statuses
+            return new[] { CheckTempStorage() };
+        }
+
+        public override HealthCheckStatus ExecuteAction(HealthCheckAction action)
+        {
+            throw new InvalidOperationException("UmbracoPathCheck has no executable actions");
+        }
+
+        private static HealthCheckStatus CheckTempStorage()
+        {
+            var umbracoVersion = WebConfigurationManager.AppSettings["umbracoConfigurationStatus"].Split('.');
+
+            var umbMajorVersion = int.Parse(umbracoVersion[0]);
+            var umbMinorVersion = int.Parse(umbracoVersion[1]);
+
+            var matchingValue = umbMinorVersion >= 7 ? "EnvironmentTemp" : "true";
+            var appSetting = umbMinorVersion >= 7
+                ? "umbracoLocalTempStorage"
+                : umbMinorVersion >= 6
+                    ? "umbracoContentXMLStorage"
+                    : "umbracoContentXMLUseLocalTemp";
+
+            var tempStorageSetting = WebConfigurationManager.AppSettings[appSetting];
+
+            var isCorrectValue = string.Equals(tempStorageSetting, matchingValue, StringComparison.InvariantCultureIgnoreCase);
+
+            return
+                new HealthCheckStatus(isCorrectValue ? "Success" : $"{appSetting} should be set to '{matchingValue}', but is currently set to '{tempStorageSetting}'")
+                {
+                    ResultType = isCorrectValue ? StatusResultType.Success : StatusResultType.Error,
+                    Actions = new List<HealthCheckAction>()
+                };
+        }
+
+    }
+}
