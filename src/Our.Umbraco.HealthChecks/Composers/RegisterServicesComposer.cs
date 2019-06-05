adc38e6d8697c7420dbb017c0452de7e14d695cd
Paul Seal <prjseal@gmail.com>
2019-06-06 00:17:54 +0100

    - renamed compose folder to composers

--- src/Our.Umbraco.HealthChecks/Composers/RegisterServicesComposer.cs
+++ src/Our.Umbraco.HealthChecks/Composers/RegisterServicesComposer.cs
@@ -3,7 +3,7 @@ using Umbraco.Core.Composing;
 using Umbraco.Core.Services;
 using Umbraco.Core.Services.Implement;
 
-namespace Our.Umbraco.HealthChecks.Compose
+namespace Our.Umbraco.HealthChecks.Composers
 {
     [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
     public class RegisterServicesComposer : IUserComposer
